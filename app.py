d3ad7bda3aabe16d0e3bc1f14fd0f48bea997bdc
kumarrohan1386-eng <kumarrohan1386@gmail.com>
2026-03-23 22:51:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+# app.py
+
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import joblib
+
+# Load the trained model
+model = joblib.load("KNN_heart.pkl")
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Extract data from form
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    
+    # Make prediction
+    prediction = model.predict(final_features)
+    output = 'Placed' if prediction[0] == 1 else 'Not Placed'
+
+    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+ 
